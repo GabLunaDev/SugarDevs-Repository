e1830da17e469a5ff54d86ae5c0b65a1c7903cf7
GUSTAVO PEREIRA OLIVEIRA LIMA <gustavo.00000844518@unicap.br>
2023-05-19 23:16:36 +0000

    changed app just so it could be visible the ussage of POLYMORPHISM in our code :)

--- src/App.java
+++ src/App.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) { // idk where to start (gab got sick today) ~gus 27/04
-        Scanner in = new Scanner (System.in);
-        String keepgoing = "YES";
+        User user = new User("John", "Doe", 25, "john@example.com", "123456789", true, false, 70.0, 180.0, "Type 1");
+        printPersonDetails(user); // Polymorphism in action
 
-        while(keepgoing.equals("YES")){
-            // add stuff
+    }
 
-            System.out.println("Would you like to repeat the process or do it again?(Yes or No)");
-            keepgoing = in.nextLine().toUpperCase(); 
-        }
-        
-        
+    public static void printPersonDetails(Person person) {
+        System.out.println("Name: " + person.getFirstName() + " " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
     }
 }
